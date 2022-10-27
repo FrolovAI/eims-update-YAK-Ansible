d250f0ab18c3622426edea5a293aa0ba3894396c
FrolovAI <116149002+FrolovAI@users.noreply.github.com>
2022-10-27 08:42:02 +0300

    Add files via upload

---
+++ 1_ssh-key.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+ssh-keygen
+ssh-copy-id user@localhost
+ssh-copy-id user@100.100.201.159
+ssh-copy-id user@100.100.201.167
---
+++ UPDATE_EIMS_AUTO.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+ p=`lftp -e "cls --sort=date /eims_versions/astra-1.6/eims-Elina | head -1 && exit" 100.100.105.110`
+ echo "Скачивается версия ИМС:"
+ echo $p
+ lftp -e "get $p -o eims.tar.gz && exit" 100.100.105.110
+# ansible-playbook eims-update.yml
+# rm ./eims.tar.gz
+ echo "----------Завершено----------"
